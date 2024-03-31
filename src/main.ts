f09e2063efe27a6ec16d7a32f6292345fd9059ce
Ynon Perek <ynonp@Ynons-MacBook-Air.local>
2024-03-31 22:11:03 +0300

    step1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import process from 'node:process';
 import root from './routes/root.ts';
 import path from 'node:path';
 import url from 'node:url';
+import createError from 'http-errors';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -13,7 +14,15 @@ app.set('views', path.join(dirname, '/views'));
 
 app.use('/', root);
 app.use(express.static('./public'));
-app.set()
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createError(404, 'Not Found'));
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).send({ errors: [{ message: "Something went wrong" }] });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
