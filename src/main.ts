392d721bf794b070124b459c2e40dd033b58b0aa
Ynon Perek <ynonp@Ynons-MacBook-Air.local>
2024-03-31 22:30:39 +0300

    fixed static files

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import process from 'node:process';
 import root from './routes/root.ts';
 import path from 'node:path';
 import url from 'node:url';
+import buffer from 'node:buffer';
 import createError from 'http-errors';
+import { contentType } from "https://deno.land/std@0.221.0/media_types/mod.ts";
+import { setgroups } from "node:process";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -13,7 +16,20 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(dirname, '/views'));
 
 app.use('/', root);
-app.use(express.static('./public'));
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userInput = path.join('./public', req.path);
+    const safeInput = path.normalize(userInput).replace(/^(\.\.(\/|\\|$))+/, '');
+    const mime = contentType(path.extname(safeInput))
+    console.log(`File: ${safeInput}. content type = ${mime}`);
+    const content = await Deno.readFile(safeInput);
+    res.set('Content-Type', mime);
+    res.send(buffer.Buffer.from(content));  
+  } catch (err) {
+    console.log(`file not found ${req.path}`);
+    next();
+  }
+});
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   next(createError(404, 'Not Found'));
